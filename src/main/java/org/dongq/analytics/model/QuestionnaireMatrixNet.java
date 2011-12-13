2e81d119f372687214b38b58430b7d94eeaa4036
eastseven <eastseven@foxmail.com>
2011-12-13 16:08:36 +0800

    提交

---
+++ src/main/java/org/dongq/analytics/model/QuestionnaireMatrixNet.java
@@ -0,0 +1,15 @@
+package org.dongq.analytics.model;
+
+public class QuestionnaireMatrixNet extends QuestionnairePaper {
+
+	private long relationPersonId;
+
+	public long getRelationPersonId() {
+		return relationPersonId;
+	}
+
+	public void setRelationPersonId(long relationPersonId) {
+		this.relationPersonId = relationPersonId;
+	}
+
+}
